a0aa7b1c2aa8438cd48091ac81c68a0f4555032b
V1SHAL421 <vishalmkumar3@gmail.com>
2025-07-05 18:06:39 +0100

    feat: add exception handling to setup_llm function:

--- src/utils/setup_llm.py
+++ src/utils/setup_llm.py
@@ -3,14 +3,21 @@ import os
 from langchain_openai import ChatOpenAI
 
 def setup_openai_llm():
-    load_dotenv()
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0.2,
-        max_tokens=1000,
-        verbose=True,
-        timeout=None,
-        api_key=os.getenv("OPENAI_API_KEY")
-    )
+    try:
+        load_dotenv()
 
-    return llm
+        assert os.getenv("OPENAI_API_KEY") is not None, ("OPENAI_API_KEY is not set. Please set it in .env file")
+
+        llm = ChatOpenAI(
+            model_name="gpt-3.5-turbo",
+            temperature=0.2,
+            max_tokens=1000,
+            verbose=True,
+            timeout=None,
+            api_key=os.getenv("OPENAI_API_KEY")
+        )
+
+        return llm
+    
+    except Exception as e:
+        print(f"Exception occurred when setting up Open AI LLM model: {e}")
